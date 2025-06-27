50e3cc7e6b87b0216745858c1351b8d8c38fa3a7
Tejaswi Ravavarapu <159167179+TejaswiR0912@users.noreply.github.com>
2025-06-27 23:27:38 +0530

    Task 4.sql

--- Task 4.sql
+++ Task 4.sql
@@ -1,19 +1,41 @@
 create database task4;
 use task4;
+create database db;
+use db;
+drop TABLE employee_salary;
 CREATE TABLE employee_salary (
-    Emp_ID INT PRIMARY KEY,
+    Emp_ID INT,
     Emp_Name VARCHAR(100),
     Department VARCHAR(50),
+    Points int null,
     Salary DECIMAL(10, 2),
-    Bonus DECIMAL(10, 2),
-    Join_Date DATE
+    Bonus DECIMAL(10, 2)
 );
 
-INSERT INTO employee_salary (Emp_ID, Emp_Name, Department, Salary, Bonus, Join_Date) VALUES
-(1, 'Manya', 'HR', 50000, 5000, '2021-01-15'),
-(2, 'Shiva', 'IT', 60000, 7000, '2020-03-10'),
-(3, 'Charan', 'HR', 52000, 4500, '2022-06-01'),
-(4, 'Latha', 'Finance', 55000, 6000, '2019-11-25'),
-(5, 'Esha', 'IT', 62000, 6500, '2023-02-14');
+INSERT INTO employee_salary 
+VALUES(1, 'Manya', 'HR',7, 50000, 5000),
+(2, 'Shiva', 'IT',2, 60000, 7000),
+(3, 'Charan', 'HR', null,52000, 4500),
+(4, 'Latha', 'Finance', 8,55000, 6000),
+(5, 'Esha', 'IT', 6, 52000,6850);
 
+select * from employee_salary;
+
+select count(*) from employee_salary;
+
+select count(points) from employee_salary;
+
+select count(*) from employee_salary where Department='HR';
+
+select max(Salary) from employee_salary;
+
+select min(Bonus) from employee_salary; 
+
+select sum(points) from employee_salary; 
+
+select avg(salary) from employee_salary; 
+
+select department,sum(bonus) from employee_salary group by department; 
+
+select Emp_ID, avg(salary) from employee_salary group by Emp_ID having Emp_ID=5;
 
